575e73b08dc3afb45ccec5def79272d6c0ecd578
alihandgrmnclr <alihandegirmenciler@gmail.com>
2023-11-05 15:50:52 +0300

    refactor: toggle button absolute removed

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,12 +14,12 @@ import Navbar from "@/components/Navbar"
 
 <style scoped>
 .page {
-  @apply w-full h-[100vh-40px] justify-center items-center;
+  @apply w-full justify-center items-center;
 }
 .container {
   @apply justify-center w-[80%] mx-auto;
 }
 .toggle {
-  @apply sm:absolute my-2 bottom-0 left-[50%] translate-x-[-50%];
+  @apply my-2 bottom-0 left-[50%] translate-x-[-50%];
 }
 </style>
